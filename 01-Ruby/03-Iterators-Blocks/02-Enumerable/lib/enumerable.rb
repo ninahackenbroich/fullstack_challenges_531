4ca131d6af6425b611c4a77e4b0c16ec32fd5043
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2021-01-11 10:49:34 +0100

    array methods

--- 01-Ruby/03-Iterators-Blocks/02-Enumerable/lib/enumerable.rb
+++ 01-Ruby/03-Iterators-Blocks/02-Enumerable/lib/enumerable.rb
@@ -1,34 +1,40 @@
 def sum_odd_indexed(array)
-  # TODO: computes the sum of elements at odd indexes (1, 3, 5, 7, etc.)
+  num = 0
+  array.each_with_index { |x, i| i.odd? ? num += x : num += 0 }
+  return num # TODO: computes the sum of elements at odd indexes (1, 3, 5, 7, etc.)
   #       You should use Enumerable#each_with_index
 end
 
 def even_numbers(array)
-  # TODO: Return the even numbers from a list of integers.
+  array.select { |x| x.even? } # TODO: Return the even numbers from a list of integers.
   #       You should use Enumerable#select
 end
 
 def short_words(array, max_length)
+  array.reject { |word| word.length > max_length }
   # TODO: Take an array of words, return the array of words not exceeding max_length characters
   #       You should use Enumerable#reject
 end
 
 def first_under(array, limit)
-  # TODO: Return the first number from an array that is less than limit.
+  array.find { |x| x < limit } # TODO: Return the first number from an array that is less than limit.
   #       You should use Enumerable#find
 end
 
 def add_bang(array)
+  array.map { |str| str + "!" }
   # TODO: Take an array of strings and return a new array with "!" appended to each string.
   #       You should use Enumerable#map
 end
 
 def concatenate(array)
-  # TODO: Concatenate all strings given in the array.
+  array.reduce(:+) # TODO: Concatenate all strings given in the array.
   #       You should use of Enumerable#reduce
 end
 
 def sorted_pairs(array)
-  # TODO: Reorganize an array into slices of 2 elements, and sort each slice alphabetically.
+  arr = []
+  array.each_slice(2) { |a| arr << a.sort }
+  arr # TODO: Reorganize an array into slices of 2 elements, and sort each slice alphabetically.
   #       You should use of Enumerable#each_slice
 end
