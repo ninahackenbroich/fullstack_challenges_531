ec0af4eb27309cb0a5e9beb01f02f5231be74ed4
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2021-01-11 14:21:49 +0100

    burger meister

--- 01-Ruby/03-Iterators-Blocks/03-Burger-restaurant/lib/burger_restaurant.rb
+++ 01-Ruby/03-Iterators-Blocks/03-Burger-restaurant/lib/burger_restaurant.rb
@@ -1,3 +1,9 @@
 def burger(patty, sauce, topping)
-  # TODO: code the `burger` method
+  burger = []
+  burger << "bread"
+  block_given? ? burger << yield(patty) : burger << patty
+  burger << sauce
+  burger << topping
+  burger << "bread"
+  burger # TODO: code the `burger` method
 end
