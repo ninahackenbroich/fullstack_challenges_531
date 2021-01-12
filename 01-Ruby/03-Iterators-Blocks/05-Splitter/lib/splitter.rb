9786bfc96b6808cee54dd1276219169f28b6aff1
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2021-01-12 10:58:52 +0100

    splitter method defined

--- 01-Ruby/03-Iterators-Blocks/05-Splitter/lib/splitter.rb
+++ 01-Ruby/03-Iterators-Blocks/05-Splitter/lib/splitter.rb
@@ -1,10 +1,17 @@
 def size_splitter(array, size)
+  one = array.select { |word| word.length == size }
+  two = array.reject { |word| word.length == size }
   # TODO: Return an array of two arrays, the first containing
   #       words of length `size`, the second containing all the other words
-  #       In addition to this split, each array should be *sorted*.
+  [one.sort, two.sort] # In addition to this split, each array should be *sorted*.
 end
 
 def block_splitter(array)
+  one = array.select { |word| yield(word) }
+  two = array.reject { |word| yield(word) }
+  # TODO: Return an array of two arrays, the first containing
+  #       words of length `size`, the second containing all the other words
+  [one.sort, two.sort]
   # TODO: Return an array of two arrays, the first containing
   #       elements for which the given block yields true,
   #       the second containing all the other elements.
