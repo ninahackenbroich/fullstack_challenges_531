df793404c4b83c0045fbdb812896a150f1c929cd
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2021-01-12 12:49:14 +0100

    check if palindrome

--- 01-Ruby/03-Iterators-Blocks/Optional-01-Playing-with-strings/lib/palindrome.rb
+++ 01-Ruby/03-Iterators-Blocks/Optional-01-Playing-with-strings/lib/palindrome.rb
@@ -1,3 +1,8 @@
 def palindrome?(a_string)
-  # TODO: check if a_string is a palindrome
+  if a_string.empty?
+    false
+  else
+    a_string.gsub(/\W/, "").gsub(/\d/, "").downcase == a_string.gsub(/\W/, "").gsub(/\d/, "").downcase.reverse
+    # TODO: check if a_string is a palindrome
+  end
 end
