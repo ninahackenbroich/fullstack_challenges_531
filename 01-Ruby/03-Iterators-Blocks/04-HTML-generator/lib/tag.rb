d099864ed64651e4e3feb212014e47175538bfc4
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2021-01-12 10:17:48 +0100

    html generator

--- 01-Ruby/03-Iterators-Blocks/04-HTML-generator/lib/tag.rb
+++ 01-Ruby/03-Iterators-Blocks/04-HTML-generator/lib/tag.rb
@@ -1,4 +1,12 @@
 def tag(tag_name, attributes = nil)
+  content = yield if block_given?
+  if attributes.nil? == false
+    attr_tag = "#{attributes[0]}=\"#{attributes[1]}\""
+    "<#{tag_name} #{attr_tag}>#{content}</#{tag_name}>"
+  else
+
+    "<#{tag_name}>#{content}</#{tag_name}>"
+  end
   # TODO: Build HTML tags around content given in the block
   #       The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
 end
