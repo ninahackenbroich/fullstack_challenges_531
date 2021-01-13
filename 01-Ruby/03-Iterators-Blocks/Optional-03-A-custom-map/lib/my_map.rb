c9f12ada11cb0c3b3b56485d81177103952b743f
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2021-01-13 11:33:58 +0100

    my own map

--- 01-Ruby/03-Iterators-Blocks/Optional-03-A-custom-map/lib/my_map.rb
+++ 01-Ruby/03-Iterators-Blocks/Optional-03-A-custom-map/lib/my_map.rb
@@ -1,3 +1,6 @@
 def my_map(array)
+  new_array = []
+  array.each { |element| new_array << yield(element) }
+  new_array
   # TODO: Re-implement the same behavior as `Enumerable#map` without using it! You can use `#each` though.
 end
