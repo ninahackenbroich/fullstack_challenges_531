77c56fa9cadcd243cd4fae53878b63f644915e35
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2021-01-13 11:01:52 +0100

    message with blocks

--- 01-Ruby/03-Iterators-Blocks/Optional-02-Storing-blocks-in-Proc/lib/love_message.rb
+++ 01-Ruby/03-Iterators-Blocks/Optional-02-Storing-blocks-in-Proc/lib/love_message.rb
@@ -1,8 +1,11 @@
 def tell(who, &message_blk)
+  message = message_blk.call
+  "#{who}, #{message}!"
   # TODO: return message addressed to who
 end
 
 def tell_mum(&message_blk)
+  tell("mum", &message_blk)
   # TODO: return message addressed to your mum
   # => Should call #tell of course
 end
@@ -11,11 +14,14 @@ end
 # tell_mum {"I love you"}
 
 def tell_with_proc(who, message_blk)
+  message = message_blk.call
+  "#{who}, #{message}!"
   # TODO: return message addressed to who
   # => Message is passed as Proc, not as block
 end
 
 def tell_mum_with_proc(message_blk)
+  tell_with_proc("mum", message_blk)
   # TODO: return message addressed to your mum
   # => Should call #tell_with_proc of course
 end
